e50f9ac84efe8e3b7c4c428fed1c095a4ead3f6d
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-06-04 14:04:00 -0500

    Mostrando información del producto

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,28 @@ import { useState } from "react";
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({children}) => {
+    
+    //Shopping cart - count
     const [count, setCount] = useState(0);
+
+    //Product detail - open/close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    //Product detail - show product
+    const [productToShow, setProductToShow] = useState({});
+
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
